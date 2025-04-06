8d99204d8624ae409fe46837d7e64e3f54a0d031
krishan kant choudhary <83417448+krishankantchoudhary@users.noreply.github.com>
2025-04-06 14:26:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let button=document.getElementById("incrementBtn");
+let count=document.getElementById("counter");
+button.addEventListener("click",function(){
+	let currentvalue=parseInt(count.textContent);
+	 alert("Current value before increment: " + currentValue);
+	count.textContent=currentvalue+1;
+})
